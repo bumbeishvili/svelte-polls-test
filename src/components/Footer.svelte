869b6fa6d3c67c519ce8313b0ee0f4321fd7de13
bumbeishvili <me@davidb.dev>
2022-07-14 23:00:02 +0400

    header and footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,19 @@
+<script></script>
+
+<footer>
+ <div class="copyright">Copyright  2020 Poll Ninja</div>
+</footer>
+
+<style>
+ footer {
+  padding: 40px;
+  text-align: center;
+ }
+ .copyright {
+  font-size: 14px;
+  display: inline-block;
+  color: #aaa;
+  padding: 20px;
+  border-top: 1px solid #ddd;
+ }
+</style>
